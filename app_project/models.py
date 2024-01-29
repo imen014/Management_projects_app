4f9b755dee7c79c8346f8e9b048acf3ca1f3bf1e
imen rezgui <rezgi.imen1093u@gmail.com>
2024-01-29 14:29:36 +0100

    second commit

--- app_project/models.py
+++ app_project/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Permission
+from django.contrib.auth.models import AbstractUser, Permission, Group
 from django.contrib.auth import get_user_model
 
 
@@ -16,6 +16,7 @@ class CreatorProjectModel(AbstractUser):
     email = models.EmailField()
     role = models.CharField(max_length=50,choices=CHOICES)
 
+       
 class PermissionCreator(Permission):
     pass
 
@@ -23,3 +24,15 @@ class PermissionCreator(Permission):
 class AttribuerPermission(models.Model):
     permission = models.ForeignKey(PermissionCreator, on_delete=models.CASCADE)
     users = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+class GroupCreator(Group):
+    pass
+
+class AttribuerGroupModel(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    group = models.ForeignKey(GroupCreator, on_delete=models.CASCADE)
+
+class AttribuerGroupPermission(models.Model):
+    group = models.ForeignKey(GroupCreator, on_delete=models.CASCADE)
+    permission = models.ForeignKey(PermissionCreator, on_delete=models.CASCADE)
